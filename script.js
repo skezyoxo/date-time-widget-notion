3a817153ea9ad968b4a21345f626d7cd37698c66
skezyoxo <152003424+skezyoxo@users.noreply.github.com>
2023-12-16 23:46:37 +1000

    script.js

--- script.js
+++ script.js
@@ -11,10 +11,17 @@ function updateTimeAndDate() {
 
     var timeString = hours + ":" + minutes + ":" + seconds + " " + ampm;
 
-    var date = now.toDateString();
+    // Formatting the date as "Day, DD Month"
+    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var dayName = days[now.getDay()];
+    var date = now.getDate();
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var monthName = months[now.getMonth()];
+
+    var dateString = dayName + ", " + date + " " + monthName;
 
     document.getElementById("time").innerText = timeString;
-    document.getElementById("date").innerText = date;
+    document.getElementById("date").innerText = dateString;
 
     setTimeout(updateTimeAndDate, 1000);
 }
