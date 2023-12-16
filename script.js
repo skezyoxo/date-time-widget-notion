4e68904cc6f13558efb49ae85db86a5b3ed1fab7
skezyoxo <152003424+skezyoxo@users.noreply.github.com>
2023-12-16 23:39:00 +1000

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 function updateTimeAndDate() {
     var now = new Date();
-    var hours = String(now.getHours()).padStart(2, '0');
+    var hours = now.getHours();
     var minutes = String(now.getMinutes()).padStart(2, '0');
     var seconds = String(now.getSeconds()).padStart(2, '0');
-    var timeString = hours + ":" + minutes + ":" + seconds;
+
+    // Convert to 12-hour format
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+
+    var timeString = hours + ":" + minutes + ":" + seconds + " " + ampm;
 
     var date = now.toDateString();
 
